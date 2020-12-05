d5f0628b44272e3b940bc6b498a9687c7f170f70
Blair Replogle <blair.replogle@gmail.com>
2020-12-05 10:13:32 -0700

    day 4: part 2

--- 2020/4/passport.swift
+++ 2020/4/passport.swift
@@ -17,7 +17,7 @@ func main() {
 		let response = PassportResponse(input: input)
 		print(response)
 
-		let validResponses = response.passports.filter { $0.isPartOneValid }
+		let validResponses = response.passports.filter { $0.isValid }
 		print("Valid Responses: \(validResponses.count)")
 	} catch {
 		print("failed to get input")
@@ -83,17 +83,30 @@ extension PassportResponse {
 			self.fields = map
 		}
 
-		var isPartOneValid: Bool {
-			guard fields.keys.count >= 7 else { return false }
-			guard let _ = fields[Kind.ecl.rawValue] else { return false }
-			guard let _ = fields[Kind.pid.rawValue] else { return false }
-			guard let _ = fields[Kind.eyr.rawValue] else { return false }
-			guard let _ = fields[Kind.hcl.rawValue] else { return false }
-			guard let _ = fields[Kind.byr.rawValue] else { return false }
-			guard let _ = fields[Kind.iyr.rawValue] else { return false }
-			guard let _ = fields[Kind.hgt.rawValue] else { return false }
+		var isValid: Bool {
+			return isPartOneValid && isPartTwoValid
+		}
 
-			return true
+		var isPartOneValid: Bool {
+			var requiredKinds = Set(Kind.allCases.filter { $0 != .cid })
+			let included = Set(fields.keys.compactMap { Kind(rawValue: $0) })
+			let subtracted = requiredKinds.subtracting(included)
+			let isValid = subtracted.count == 0
+
+			return isValid
+		}
+
+		var isPartTwoValid: Bool {
+			let truths = fields.map { (key, value) -> Bool in
+				guard let kind = Kind(rawValue: key) else { return false }
+
+				let truth = kind.isValid(value: value)
+				return truth
+			}
+
+			let isValid = !truths.contains(false)
+
+			return isValid
 		}
 	}
 }
@@ -104,7 +117,50 @@ extension PassportResponse.Passport {
 	enum Kind: String, CaseIterable {
 		case ecl, pid, eyr, hcl
 		case byr, iyr, cid, hgt
+
+		func isValid(value: String) -> Bool {
+			switch self {
+				case .ecl:
+					return EyeColor(rawValue: value) != nil
+				case .pid:
+					return value.count == 9 && Int(value) != nil
+				case .eyr:
+					let number = Int(value) ?? 0
+					return 2020...2030 ~= number
+				case .hcl:
+					return value.isHexColor
+				case .byr:
+					let number = Int(value) ?? 0
+					return 1920...2002 ~= number
+				case .iyr:
+					let number = Int(value) ?? 0
+					return 2010...2020 ~= number
+				case .cid:
+					return true
+				case .hgt:
+					if value.contains("in") {
+						let numberString = value.replacingOccurrences(of: "in", with: "")
+						let number = Int(numberString) ?? 0
+						return 59...76 ~= number
+					}
+
+					if value.contains("cm") {
+						let numberString = value.replacingOccurrences(of: "cm", with: "")
+						let number = Int(numberString) ?? 0
+						return 150...193 ~= number
+					}
+
+					return false
+			}
+
+			return true
+		}
 	}
+}
+
+
+enum EyeColor: String {
+	case amb, blu, brn, gry, grn, hzl, oth
 }
 
 
@@ -115,5 +171,18 @@ extension Array {
         }
 
         return self[index]
+    }
+}
+
+
+extension String {
+    var isHexColor: Bool {
+        guard self.hasPrefix("#") else { return false }
+
+        let characters = Array(self.dropFirst())
+        let isHex = characters.map { $0.isHexDigit }
+        let result = !isHex.contains(false)
+
+        return result
     }
 }
